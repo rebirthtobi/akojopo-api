cb775b0f4f4ebeb3851f938b2fab53a2bd7637a6
Tobi <ttobi4@gmail.com>
2018-01-17 12:29:19 +0100

    fixing database connection

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    DATABASE: 'mongodb://rebirthtobi:76642344@ds157057.mlab.com:57057/akojopo',
+    DATABASE: 'mongodb://akojopo-user:akojopo@ds157057.mlab.com:57057/akojopo',
     ENCRYPTION_KEY: 'qzWLTr/JRsbiARfB/WPnq+du2dyj/p71P/TkoXJ5//s=',
     AUTH_SECRET_KEY: 'r5y4nVUt3L5rqjEcppqa+JUPT276MZo3pT14asSTsVA=',
     MEETUP_API_KEY: '4e2e232644512452758583413771259',
     EVENTBRITE_TOKEN: 'TL4L2FOKRUKU7CLIJTS2'
 };
+
+//https://api.meetup.com/2/open_events?key=4e2e232644512452758583413771259&sign=true&photo-host=public&country=ng&city=lagos&category=34&page=20
